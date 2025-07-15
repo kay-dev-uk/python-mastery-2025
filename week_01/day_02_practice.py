52bdaac7488f0ec3934dc3bfa4f90906e90ccc37
kay-dev-uk <kay.dev.uk@gmail.com>
2025-07-15 09:00:03 +0100

    Week 1 Day 2 - Initial commit

---
+++ week_01/day_02_practice.py
@@ -0,0 +1,13 @@
+### Day 2 Exercises ###
+
+### Prints the result with float formatting .format() ###
+result = 560/223
+
+print("Result is {r:1.2f}".format(r=result))
+
+### F strings ###
+
+print(f'Hey, the result is {result}')
+
+
+
