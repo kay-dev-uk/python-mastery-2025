200f75182a77a2c3ddc154f13454007b33a84625
kay-dev-uk <kay.dev.uk@gmail.com>
2025-07-16 19:51:04 +0100

    Guess the number - Refactor. Added error handling.

--- week_01/guess_the_number.py
+++ week_01/guess_the_number.py
@@ -13,20 +13,39 @@ from random import randint
 def guess_the_number():
     print("Welcome!")
     keep_playing = True
-    x = randint(1,11)
-    user_guess = 0
-    while user_guess != x and keep_playing:
-        user_guess = int(input('Guess the number from 1 to 10: '))
-        if(user_guess < x):
-            print('Too low!')
-        elif(user_guess > x):
-            print('Too high!')
-        else:
-            user_response = input('Congrats! You won! Play again? Y/N: ')
-            if(user_response == 'y'):
-                guess_the_number()
-            else:
+    while keep_playing:
+        x = randint(1,10)
+        user_guess = 0
+        while user_guess != x and keep_playing:
+            try:
+                user_guess = int(input('Guess the number from 1 to 10: '))
+                if(user_guess < x):
+                    print('Too low!')
+                elif(user_guess > x):
+                    print('Too high!')
+                else:
+                    valid_response = False
+                    while not valid_response:
+                        user_response = str(input('Congrats! You won! Play again? Y/N: '))
+                        if(user_response.lower() == 'y'):
+                            valid_response = True
+                            break
+                        elif(user_response.lower() == 'n'):
+                            keep_playing = False
+                            valid_response = True
+                            break
+                        else:
+                            print('Invalid Response. Enter Y or N')
+                            pass
+            except ValueError:
+                print('It is not a valid response. Please, try again')
+            except EOFError:
+                print('\nExiting game...')
                 keep_playing = False
                 break
+        if not keep_playing:
+            break    
+
+
 
 guess_the_number()
