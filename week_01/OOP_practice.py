ea424a09101ff48f938544b2955072f3e711ba34
kay-dev-uk <kay.dev.uk@gmail.com>
2025-07-17 22:28:02 +0100

    OOP first lesson

---
+++ week_01/OOP_practice.py
@@ -0,0 +1,23 @@
+class NameOfClass():
+    def __init__(self,param1,param2):
+        self.param1 = param1
+        self.param2 = param2
+
+    def method(self):
+        print(self.param1)
+
+
+class Sample():
+    pass
+
+my_sample = Sample()
+
+class Dog():
+    def __init__(self, breed, name, spots):
+        self.breed = breed
+        self.name = name
+        self.spots = spots
+
+
+my_dog = Dog(breed='Lab', name='Felix', spots=False)
+
