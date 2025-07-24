946466d4061dc83aa72f7a58da6ab5104147da11
kay-dev-uk <kay.dev.uk@gmail.com>
2025-07-24 23:45:38 +0100

    Had a hard time installing dotenv

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -2,6 +2,8 @@
 
 # Command-line application that takes user input of a city name and displays the current weather for that city.
 #API is weatherapi.com
+from dotenv import load_dotenv
 
-
-
+class WeatherService():
+    def __init__(self, api_key):
+        self.api_key = api_key
