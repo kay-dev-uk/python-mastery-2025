cacece5f38c5fb6d9b8bf6d5d91b738e7558fea7
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-12 16:20:11 +0100

    Hangman first steps

--- September/hangman.py
+++ September/hangman.py
@@ -1,1 +1,16 @@
-print("Let's play")
+import random
+word_list = ['aardvark', 'baboon', 'camel']
+
+keep_playing = True
+
+
+
+
+while keep_playing:
+    secret_word = random.choice(word_list)
+    show_word = ''
+    guess = input('Guess the letter: \n')
+    if guess in secret_word:
+        print('Right')
+    else:
+        print('Wrong')
