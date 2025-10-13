25da0c1295213e07237763829ccef0f227d33401
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-13 23:06:53 +0100

    Add lives, clean the code

--- September/hangman.py
+++ September/hangman.py
@@ -6,19 +6,24 @@ keep_playing = True
 secret_word = random.choice(word_list)
 display = len(secret_word) * '_'
 guessed_letters = []
+user_lives = 5
 
 while keep_playing:
     
     print(display)
+    print(f'Lives left: {user_lives}')
 
     guess = input('Guess the letter: \n').lower()
     if guess in secret_word:
+        guessed_letters.append(guess)
         print('Correct!')
     else:
         print('Incorrect!')
-    for letter in secret_word:
-        if guess == letter:
-            guessed_letters.append(guess)
+        user_lives -= 1
+        if user_lives == 0:
+            print('You lost!')
+            keep_playing = False
+
     if len(guessed_letters) > 0:
         display = ''
         for i in secret_word:
@@ -26,10 +31,9 @@ while keep_playing:
                 display += i
             else:
                 display += "_"
-    if '_' in display:
-        keep_playing = True
-    else:
-        print('You won!')
-        keep_playing = False
+        if '_' not in display:
+            print(secret_word)
+            print('You won!')
+            keep_playing = False
 
 
