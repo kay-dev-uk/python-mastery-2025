1bdf3044cb7d484ee02d23d0675205ddad1f8ece
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-13 21:52:47 +0100

    Progress in Hangman

--- September/hangman.py
+++ September/hangman.py
@@ -3,14 +3,25 @@ word_list = ['aardvark', 'baboon', 'camel']
 
 keep_playing = True
 
-
-
+secret_word = random.choice(word_list)
+display = len(secret_word) * '_'
+guessed_letters = []
 
 while keep_playing:
-    secret_word = random.choice(word_list)
-    show_word = ''
-    guess = input('Guess the letter: \n')
-    if guess in secret_word:
-        print('Right')
-    else:
-        print('Wrong')
+    
+    print(display)
+
+    guess = input('Guess the letter: \n').lower()
+    for letter in secret_word:
+        if guess == letter:
+            guessed_letters.append(guess)
+
+    if len(guessed_letters) > 0:
+        display = ''
+        for i in secret_word:
+            if i in guessed_letters:
+                display += i
+            else:
+                display += "_"
+
+
