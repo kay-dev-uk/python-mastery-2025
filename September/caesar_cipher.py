8de8c7831dbc3b631d74b8793c7447934d0c9c7c
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-16 23:28:36 +0100

    Combine Encrypt/Decrypt

--- September/caesar_cipher.py
+++ September/caesar_cipher.py
@@ -2,25 +2,27 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 alphabet *= 1000
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ''
-    for letter in original_text.lower():
-        if letter == ' ':
-            encrypted_text += letter
-        else:
-            encrypted_text += alphabet[alphabet.index(letter) + shift_amount]
-    
-    print(encrypted_text)
+def caesar(direction, text, shift):
 
-def decrypt(encrypted_text, shift_amount):
-    decrypted_text = ''
-    for letter in encrypted_text:
-        if letter == ' ':
-            decrypted_text += letter
-        else:
-            decrypted_text += alphabet[alphabet.index(letter) - shift_amount]
-    
-    print(decrypted_text)
+    if direction == 'encrypt':
+        encrypted_text = ''
+        for letter in text.lower():
+            if letter == ' ':
+                encrypted_text += letter
+            else:
+                encrypted_text += alphabet[alphabet.index(letter) + shift]
+        
+        print(encrypted_text)
 
-decrypt('cvmxjolbmfcjlvt difslj mpmpm vrjxfy', 1)
-encrypt('Bulwinkalebikus Cherki LOLOl uqiwex',1)
+    else:
+        decrypted_text = ''
+        for letter in encrypted_text:
+            if letter == ' ':
+                decrypted_text += letter
+            else:
+                decrypted_text += alphabet[alphabet.index(letter) - shift]
+        
+        print(decrypted_text)
+
+# decrypt('cvmxjolbmfcjlvt difslj mpmpm vrjxfy', 1)
+caesar('encrypt','Bulwinkalebikus Cherki LOLOl uqiwex',1)
