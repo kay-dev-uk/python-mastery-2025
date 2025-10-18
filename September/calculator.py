fd86f31edd28aadf28af87a26af96fc95230e89e
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-18 17:33:36 +0100

    Calculator app initial commit

---
+++ September/calculator.py
@@ -0,0 +1,9 @@
+
+# Prompt for the first num
+# Show different options(+ - * /)
+# Prompt user to pick one
+# Prompt for second num
+# Show Result
+# Prompt for continuing using the same result number or not
+# loop again
+
