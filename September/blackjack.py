93945f8589d893b7dd5ee080370c874a78368801
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-19 22:00:01 +0100

    BlackJack initial commit

---
+++ September/blackjack.py
@@ -0,0 +1,7 @@
+# Do you want to play y/n?
+# Get 2 random cards
+# Show computer first card
+# Type y to get another card or n to pass
+# show your final hand and computer final hand (apparently bot doesnt hit)
+# print who wins
+# start over
