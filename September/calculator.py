cfa5701bf2fd2b72fdd82f1b2e9015a1e460d72f
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-19 19:36:24 +0100

    Calculator App Progress

--- September/calculator.py
+++ September/calculator.py
@@ -7,30 +7,39 @@
 # Prompt for continuing using the same result number or not
 # loop again
 
-# def calculator():
-#     "Welcome to calculator!"
+def calculator():
+    "Welcome to calculator!"
 
-#     keep_calculating = True
+    keep_calculating = True
     
-#     def calculate(first_num, operation, second_num):
-#         result = 0
-#         if operation == "+":
-#             first_num 
+    def calculate(first_num, operation, second_num):
+        result = 0
+    
+        def add(n1, n2):
+            return n1 + n2
+    
+        def subtract(n1, n2):
+            return n1 - n2
+    
+        def multiply(n1, n2):
+            return n1 * n2
+        
+        def divide(n1, n2):
+            return n1 / n2
 
-#         return()
-#         while keep_calculating:
-#             print("Please, ")
+        # while keep_calculating:
+        #     print("Please, ")
 
 # CHALLENGE
-def is_leap_year(year):
+# def is_leap_year(year):
 
-    if year % 4 != 0:
-        return False
-    if year % 100 != 0:
-        return True
-    if year % 400 != 0:
-        return False
-    return True
+#     if year % 4 != 0:
+#         return False
+#     if year % 100 != 0:
+#         return True
+#     if year % 400 != 0:
+#         return False
+#     return True
 
 
-print(is_leap_year(2100))
+# print(is_leap_year(2100))
