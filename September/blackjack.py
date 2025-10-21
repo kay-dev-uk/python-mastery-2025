e2bebe62b79b2fb7b514c946eca4c26137ed5f63
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-21 22:30:34 +0100

    Add BlackJack logic, Ace logic

--- September/blackjack.py
+++ September/blackjack.py
@@ -25,4 +25,13 @@ def blackjack():
     print(user_cards)
     print(computer_cards)
 
+    def calculate_score(cards):
+        if sum(cards) == 21 and len(cards) == 2:
+            return 0 
+        if 11 in cards and sum(cards) > 21:
+            cards.remove(11)
+            cards.append(1)
+
+        return sum(cards)
+
 blackjack()
