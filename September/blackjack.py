aa660c110c35f9dbea2b68ca0f0c4aae3cff1a3e
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-21 22:26:33 +0100

    BlackJack progress

--- September/blackjack.py
+++ September/blackjack.py
@@ -10,17 +10,19 @@ import random
 
 def blackjack():
 
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    # Standard 4 different suits
 
     user_cards = []
     computer_cards = []
 
     def deal_card():
+        cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
         return random.choice(cards)
 
-    user_cards.append(deal_card())
+    for n in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
     print(user_cards)
+    print(computer_cards)
 
 blackjack()
