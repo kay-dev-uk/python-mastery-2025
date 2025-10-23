4e8e2ac34ef2d2025c69ca32950d6cb4a33b37e4
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-23 20:40:37 +0100

    BlackJack progress

--- September/blackjack.py
+++ September/blackjack.py
@@ -5,7 +5,7 @@
 # show your final hand and computer final hand (apparently bot doesnt hit)
 # print who wins
 # start over
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 import random
 
 def blackjack():
