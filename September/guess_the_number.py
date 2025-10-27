3543e1828edda43b1fc9d826f9f110f2b93388f4
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-27 22:20:34 +0000

    Progress Guess the Number

--- September/guess_the_number.py
+++ September/guess_the_number.py
@@ -1,17 +1,28 @@
 # Guess The Number
 # Ask for difficulty easy or hard
 # Hard: 5 tries
-# Easy: unlimited
+# Easy: 10 tries
 # Upon guess if not correct print higher or lower
 
-def prime(num):
-    if num < 2:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
+# def prime(num):
+#     if num < 2:
+#         return False
+#     for i in range(2, num):
+#         if num % i == 0:
+#             return False
+#     return True
     
 
 
-print(prime(90))
+# print(prime(90))
+import random
+def game():
+    
+    difficulty = input("Select difficulty 'easy' or 'hard':\n").lower()
+    attempts = 10
+    if difficulty == 'hard':
+        attempts = 5
+    keep_playing = True
+    while keep_playing:
+        secret_number = random.randint(1,100)
+        
