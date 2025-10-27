9aeef586cff816f363b81bf7640f265e42c0d8bf
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-27 22:10:33 +0000

    Prime Number challenge

--- September/guess_the_number.py
+++ September/guess_the_number.py
@@ -3,3 +3,15 @@
 # Hard: 5 tries
 # Easy: unlimited
 # Upon guess if not correct print higher or lower
+
+def prime(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+    
+
+
+print(prime(90))
