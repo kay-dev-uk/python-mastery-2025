14c8e4f6ca391636c4f23f136f79b9ce76cfd726
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-28 23:11:43 +0000

    Finalising Guess the Number

--- September/guess_the_number.py
+++ September/guess_the_number.py
@@ -17,18 +17,39 @@
 # print(prime(90))
 import random
 def game():
-    
+    keep_playing = True
     
     while keep_playing:
         difficulty = input("Select difficulty 'easy' or 'hard':\n").lower()
         attempts = 10
         if difficulty == 'hard':
             attempts = 5
-        keep_playing = True
+        
         secret_number = random.randint(1,100)
         secret_number_guessed = False
         while not secret_number_guessed and attempts > 0:
-            user_guess = input(f"Guess the number. You have {attempts} attempts left:\n")
+            user_guess = int(input(f"Guess the number between 1 and 100. You have {attempts} attempts left:\n"))
+            if user_guess < secret_number:
+                print("Think higher")
+                attempts -= 1
+                if attempts == 0:
+                    print("You lost!")
+                    break
+            elif user_guess > secret_number:
+                print("Think lower")
+                attempts -= 1
+                if attempts == 0:
+                    print("You lost!")
+                    break
+            else:
+                print(f"You won! You guessed the number {secret_number}")
+                secret_number_guessed = True
+                break
+        user_choice = input("Play again? y/n:\n").lower()
+        if user_choice == "y":
+            continue
+        else:
+            break
         
 
 game()
