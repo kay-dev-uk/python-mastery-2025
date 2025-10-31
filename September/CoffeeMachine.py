d75fc36d9259592c7c523bf551c72cb18e5192b6
kay-dev-uk <kay.dev.uk@gmail.com>
2025-10-31 22:34:01 +0000

    Coffee Machine Initial commit

---
+++ September/CoffeeMachine.py
@@ -0,0 +1,14 @@
+# Default report:
+# 300ml Water
+# 200ml Milk
+# 100gr COffee
+# Money $0
+
+# Espress - 50ml water 18gr coffee 1.50
+# Cappucino - 250ml water 24gr coffee 100ml milk 3.00
+# Latte - 200ml water 24gr coffee 150ml Milk 2.50
+
+# Penny - 0.01 , dime - 0.10, nickel - 0.05, quarter - 0.25
+
+
+
