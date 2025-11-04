360fc8e24722b141f2a4e6ce43590fd26e57a2a4
kay-dev-uk <kay.dev.uk@gmail.com>
2025-11-04 19:48:23 +0000

    Progress Coffee Machine

--- CoffeeMachineOOP/coffeemachine.py
+++ CoffeeMachineOOP/coffeemachine.py
@@ -7,4 +7,16 @@ class CoffeMachine:
         self.coffee = coffee
         self.money = money
 
-    
+    def report(self):
+        return f"Milk: {self.milk}ml\nWater: {self.water}ml\nCoffee: {self.coffee}gr\nMoney: ${self.money}"
+
+    def check_ingridients(self, milk_amount, water_amount, coffee_amount, money_amount):
+        missing_ingridients = []
+        if milk_amount > self.milk:
+            missing_ingridients.append('milk')
+        if water_amount > self.water:
+            missing_ingridients.append('water')
+        if coffee_amount > self.coffee:
+            missing_ingridients.append('coffee')
+        if money_amount > self.money:
+            missing_ingridients.append('money')
