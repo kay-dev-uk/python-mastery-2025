008a7161aba8d36481870225db339ad9a3b20e06
kay-dev-uk <kay.dev.uk@gmail.com>
2025-11-04 18:52:22 +0000

    Add first classes

--- CoffeeMachineOOP/coffeemachine.py
+++ CoffeeMachineOOP/coffeemachine.py
@@ -1,1 +1,10 @@
 # File for Coffee Machine class
+
+class CoffeMachine:
+    def __init__(self, milk, water, coffee, money):
+        self.water = water
+        self.milk = milk
+        self.coffee = coffee
+        self.money = money
+
+    
--- CoffeeMachineOOP/drink.py
+++ CoffeeMachineOOP/drink.py
@@ -1,1 +1,10 @@
 # File for Drink Class
+
+class Drink:
+
+    def __init__(self, water, milk, coffee, price):
+        self.water = water
+        self.milk = milk
+        self.coffee = coffee
+        self.price = price
+    
